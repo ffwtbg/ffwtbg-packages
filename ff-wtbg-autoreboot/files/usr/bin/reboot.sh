fede7db00fcb909e7d3863d57febc26a70a2dc1a
Kakinger <jan@zueschen.eu>
2017-07-21 09:13:23 +0200

    changed reboot behavior

--- ff-wtbg-autoreboot/files/usr/bin/reboot.sh
+++ ff-wtbg-autoreboot/files/usr/bin/reboot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo reboot is running > /tmp/reboot.txt
-sleep $(awk 'BEGIN{srand();print int(rand()*10800)}')
+sleep $(awk 'BEGIN{srand();print int(rand()*7200)}')
 touch /etc/banner
 reboot
